28024de5d9ac17129b8e2793ac4d3417b76ba467
Damian Oguche <damianguch@yahoo.com>
2025-02-15 03:28:52 +0100

    Updated the frontend to implement User dashboard with only contacts created by user.

--- src/components/common/Dashboard.jsx
+++ src/components/common/Dashboard.jsx
@@ -4,105 +4,79 @@ import {
   Stack,
   Flex,
   Text,
-  Input,
   Badge,
-  Icon
+  Button
 } from "@chakra-ui/react";
-import { ColorModeButton } from "@/components/ui/color-mode";
 import { useColorModeValue } from "@/components/ui/color-mode";
-import CreateContactModal from "../contacts/CreateContactModal";
-import ContactGrid from "../contacts/ContactGrid";
-import { FiLogOut } from "react-icons/fi";
-import { FaSearch } from "react-icons/fa";
-import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
-import { useAuth } from "../AuthContext";
+import { FiBell, FiLogOut } from "react-icons/fi";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ContactGrid from "../contacts/ContactGrid";
+import { useAuth } from "../AuthContext";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
-const Dashboard = () => {
+const UserDashboard = () => {
+  const [user, setUser] = useState(sessionStorage.getItem("user"));
   const [contacts, setContacts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [user, setUser] = useState("");
   const { logout, isAuthenticated } = useAuth();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    setUser(sessionStorage.getItem("user"));
-  }, []);
-
-  // Centralized color and background values
-  const bgColor = useColorModeValue("white", "gray.800");
-  const textColor = useColorModeValue("gray.900", "white");
-  const dashColor = useColorModeValue("gray.200", "gray.700");
-
-  const handleSearchChange = (value) => {
-    setSearchQuery(value);
-  };
 
   const handleLogout = () => {
     logout();
     navigate("/login");
   };
 
+  // Centralized color and background values
+  const bgColor = useColorModeValue("gray.200", "gray.700");
+  const textColor = useColorModeValue("gray.900", "white");
+
   return (
     <Stack bg={{ base: "white", _dark: "#1A202C" }} minH={"100vh"}>
       <Container maxW={"900px"}>
-        <Box px={4} my={4} borderRadius={5} bg={dashColor}>
+        <Box px={4} my={4} borderRadius={5} bg={bgColor}>
           <Flex h="12" alignItems={"center"} justifyContent={"space-between"}>
             <Flex gap={3} alignItems={"center"}>
+              <Button
+                onClick={() => navigate("/dashboard/contacts")}
+                variant="outline"
+              >
+                View All Contacts
+              </Button>
               <ColorModeButton />
-              <CreateContactModal setContacts={setContacts} />
             </Flex>
-            <Flex alignItems={"center"} position={"relative"}>
-              <Input
-                bg={bgColor}
-                color={textColor}
-                placeholder="Search contacts"
-                value={searchQuery}
-                onChange={(e) => handleSearchChange(e.target.value)}
-                size="sm"
-                pl={5}
-              />
-              {!searchQuery && (
-                <Icon
-                  position={"absolute"}
-                  top={"3"}
-                  right={"3"}
+            <Flex alignItems={"center"} justifyContent={"center"} gap={2}>
+              <Box position="relative">
+                <Button variant="ghost">
+                  <FiBell />
+                </Button>
+                <Badge
+                  position="absolute"
+                  top="1"
+                  right="3"
                   color={textColor}
+                  borderRadius="full"
+                  fontSize="0.7em"
+                  bg={"red.500"}
+                  size={"xs"}
                 >
-                  <FaSearch size={"20"} />
-                </Icon>
-              )}
-            </Flex>
-
-            <Flex
-              alignItems={"center"}
-              justifyContent={"center"}
-              gap={2}
-              display={{ base: "block", sm: "flex" }}
-            >
+                  2
+                </Badge>
+              </Box>
               {isAuthenticated && (
                 <Badge
-                  display={{ base: "none", md: "block" }}
-                  bg={isAuthenticated ? "green.500" : "gray.700"}
+                  bg={isAuthenticated ? "green.500" : "gray.400"}
                   borderRadius="full"
                   size={"xs"}
-                  px={2}
+                  p={2}
                 ></Badge>
               )}
-
-              <Flex
-                gap={3}
-                alignItems={"center"}
-                display={{ base: "none", md: "block" }}
-              >
+              <Flex alignItems={"center"}>
                 <Box fontSize="md" fontWeight="semibold" color={textColor}>
                   {user}
                 </Box>
               </Flex>
-
-              <Button variant="outline">
-                <FiLogOut onClick={handleLogout} />
+              <Button variant="outline" onClick={handleLogout}>
+                <FiLogOut />
               </Button>
             </Flex>
           </Flex>
@@ -125,17 +99,19 @@ const Dashboard = () => {
             gradientTo="blue.500"
             bgClip={"text"}
           >
-            Contacts List
+            Your Contacts
           </Text>
         </Text>
+
         <ContactGrid
           contacts={contacts}
           setContacts={setContacts}
-          searchQuery={searchQuery}
+          showSearch={false}
+          userId={sessionStorage.getItem("userId")}
         />
       </Container>
     </Stack>
   );
 };
 
-export default Dashboard;
+export default UserDashboard;
